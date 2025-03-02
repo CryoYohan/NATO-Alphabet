277bbf96833a9196aa011eb812791337fdae7b3e
itsmecyril <cyrilypil@gmail.com>
2025-03-03 00:31:53 +0800

    got it, but not in correct order

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+import pandas as pd
+class NATO:
+    def __init__(self):
+        self.df = self.read_csv()
+        self.nato_dict = self.csv_to_dict()
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+    def read_csv(self):
+        self.df = pd.read_csv('nato_phonetic_alphabet.csv')
+        return self.df
 
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+    def csv_to_dict(self):
+        nato_dict = {row.letter:row.code for index,row in self.df.iterrows()}
+        return nato_dict
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+    def convert_to_nato(self, name:str):
+        name_list:list = [letter.upper() for letter in name]
+        # converted_name = [v for k,v in self.nato_dict.items() if [letter for letter in name_list if k == letter]]
+        converted_name = [v for k,v in self.nato_dict.items()if k in name_list]
+        return converted_name
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+if __name__ == "__main__":
+    nato = NATO()
+    name = 'Angela'
+    print(nato.convert_to_nato(name=name))
