4fe2d8dd2bbb76282351770d3395aba696d553b7
CryoYohan <cyrilypil@gmail.com>
2025-07-27 02:57:34 +0800

    Code cleanup, shorten code

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ import pandas as pd
 
 class NATO:
     def __init__(self):
-        self.df = self.read_csv()
-        self.nato_dict = self.csv_to_dict()
-
-    def read_csv(self):
         self.df = pd.read_csv('nato_phonetic_alphabet.csv')
-        return self.df
-
-    def csv_to_dict(self):
-        nato_dict = {row.letter:row.code for index,row in self.df.iterrows()}
-        return nato_dict
+        self.nato_dict = {row.letter:row.code for index,row in self.df.iterrows()}
 
     def convert_to_nato(self, name:str):
         letter_list:list = [letter.upper() for letter in name]
